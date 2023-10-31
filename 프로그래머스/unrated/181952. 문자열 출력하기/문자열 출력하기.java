38cc7b0a8e73facf851c487f6094a2e4d887dd87
mabyoungg <mabyoungg@gmail.com>
2023-10-31 09:40:02 +0900

    [level unrated] Title: 문자열 출력하기, Time: 175.33 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String inputWord;
+        Scanner scanner = new Scanner(System.in);
+        
+        inputWord = scanner.nextLine();
+        
+        System.out.print(inputWord);
+    }
+}
