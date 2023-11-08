3f2e6e7ce51f3245f2731d790bc20161ba346d2e
mabyoungg <mabyoungg@gmail.com>
2023-11-08 22:40:08 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -4,13 +4,13 @@ class Solution {
         int temp, k, l;
         // 0 0 0 1   1 0 1 1  2 0 2 1
         for (int i = 0; i < queries.length; i++){
-            for (int j = 0; j < queries[i].length - 1; j++){
-                k = queries[i][j];
-                l = queries[i][j+1];
-                temp = arr[k];
-                arr[k] = arr[l];
-                arr[l] = temp;
-            }
+            
+            k = queries[i][0];
+            l = queries[i][1];
+            temp = arr[k];
+            arr[k] = arr[l];
+            arr[l] = temp;
+            
         }
         
         answer = arr;
