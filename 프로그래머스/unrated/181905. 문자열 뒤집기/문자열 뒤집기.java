0b830a7ab4f89428ece9e7c182dbd50fca2552fd
mabyoungg <mabyoungg@gmail.com>
2023-11-28 09:20:32 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 9.70 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,16 +3,27 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
-        String reverse = "";
+        StringBuilder reverse = new StringBuilder(my_string.substring(s, e + 1)).reverse();
         String fistAnswer = my_string.substring(0, s);
         String lastAnswer = my_string.substring(e + 1);
-        
-        for (int i = e; i >= s; i--){
-            reverse += my_string.charAt(i);
-        }
         
         answer = fistAnswer + reverse + lastAnswer;
         
         return answer;
+        
+        
+        
+//         String answer = "";
+//         String reverse = "";
+//         String fistAnswer = my_string.substring(0, s);
+//         String lastAnswer = my_string.substring(e + 1);
+        
+//         for (int i = e; i >= s; i--){
+//             reverse += my_string.charAt(i);
+//         }
+        
+//         answer = fistAnswer + reverse + lastAnswer;
+        
+//         return answer;
     }
 }
