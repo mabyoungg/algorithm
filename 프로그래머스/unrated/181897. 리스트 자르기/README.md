d73ef21dd9e4ae6cab6a30f250a80eff70afe736
mabyoungg <mabyoungg@gmail.com>
2023-11-30 10:18:47 +0900

    [level unrated] Title: 리스트 자르기, Time: 2.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/README.md
+++ 프로그래머스/unrated/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897#) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 0.02 ms
+메모리: 74.1 MB, 시간: 2.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 10:0:13
+2023년 11월 4일 10:18:44
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -1,36 +1,67 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
-        int temp = a;
-            
+        
         if(n == 1) {
-            answer = new int[b +1 ];
-            for(int i = 0; i < answer.length; i++) {
-                answer[i] = num_list[i];
+            for(int i = 0; i < b + 1; i++) {
+                list.add(num_list[i]);
             }
         } else if(n == 2) {
-            answer = new int[num_list.length - a];
-            for(int i = 0; i < answer.length; i++) {
-                answer[i] = num_list[a];
-                a++;
+            for(int i = a; i < num_list.length; i++) {
+                list.add(num_list[i]);
             }
         } else if(n == 3) {
-            answer = new int[b - a + 1];
-            for(int i = 0; i < answer.length; i++) {
-                answer[i] = num_list[a];
-                a++;
+            for(int i = a; i < b + 1; i++) {
+                list.add(num_list[i]);
             }
         } else if(n == 4) {
-            answer = new int[(b - a) / c + 1];
-            for(int i = 0; i < answer.length; i++) {
-                answer[i] = num_list[temp];
-                temp+=c;
+            for(int i = a; i < b + 1; i+=c) {
+                list.add(num_list[i]);
             }
-        } 
+        }
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
         
         return answer;
+        
+        
+        // 1안
+//         int[] answer = {};
+//         int a = slicer[0];
+//         int b = slicer[1];
+//         int c = slicer[2];
+//         int temp = a;
+            
+//         if(n == 1) {
+//             answer = new int[b +1 ];
+//             for(int i = 0; i < answer.length; i++) {
+//                 answer[i] = num_list[i];
+//             }
+//         } else if(n == 2) {
+//             answer = new int[num_list.length - a];
+//             for(int i = 0; i < answer.length; i++) {
+//                 answer[i] = num_list[a];
+//                 a++;
+//             }
+//         } else if(n == 3) {
+//             answer = new int[b - a + 1];
+//             for(int i = 0; i < answer.length; i++) {
+//                 answer[i] = num_list[a];
+//                 a++;
+//             }
+//         } else if(n == 4) {
+//             answer = new int[(b - a) / c + 1];
+//             for(int i = 0; i < answer.length; i++) {
+//                 answer[i] = num_list[temp];
+//                 temp+=c;
+//             }
+//         } 
+        
+//         return answer;
     }
 }
