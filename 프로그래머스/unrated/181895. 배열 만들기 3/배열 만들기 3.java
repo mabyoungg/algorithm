fc8cd6ffcbcb670e8acb54ebe902ef92521ef821
mabyoungg <mabyoungg@gmail.com>
2023-11-30 10:44:32 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.13 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int size = (intervals[0][1] - intervals[0][0] + 1) +
+            (intervals[1][1] - intervals[1][0] + 1);
+        int[] answer = new int[size];
+        int count = 0;
+        
+        for(int i = intervals[0][0]; i <= intervals[0][1]; i++) {
+            answer[count] = arr[i];
+            count++;
+        }
+        
+        for(int i = intervals[1][0]; i <= intervals[1][1]; i++) {
+            answer[count] = arr[i];
+            count++;
+        }
+        
+        return answer;
+    }
+}
