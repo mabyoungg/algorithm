3c3474f24ec2e2e25e58073983df8e054a289374
mabyoungg <mabyoungg@gmail.com>
2023-12-01 23:08:14 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,16 +1,25 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length];
-        int index = 0;
         
-        for (int i = n; i < num_list.length; i++){
-            answer[index++] = num_list[i];
-        }
-        
-        for (int j = 0; j < n; j++){
-            answer[index++] = num_list[j];
-        }
+        System.arraycopy(num_list, n, answer, 0, num_list.length - n);
+        System.arraycopy(num_list, 0, answer, num_list.length - n, n);
         
         return answer;
+        
+        
+//         1안
+//         int[] answer = new int[num_list.length];
+//         int index = 0;
+        
+//         for (int i = n; i < num_list.length; i++){
+//             answer[index++] = num_list[i];
+//         }
+        
+//         for (int j = 0; j < n; j++){
+//             answer[index++] = num_list[j];
+//         }
+        
+//         return answer;
     }
 }
