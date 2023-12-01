76d6c2eea2ef247cbe01165c2a7cadda7ae786f6
mabyoungg <mabyoungg@gmail.com>
2023-12-01 17:14:25 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.11 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -3,22 +3,35 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         for(int i = 0; i < query.length; i++) {
-            List<Integer> list = new ArrayList<>();
             if(i % 2 == 0) {
-                for(int j = 0; j <= query[i]; j++) {
-                    list.add(arr[j]);
-                }
-                arr = list.stream().mapToInt(l -> l).toArray();
+                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
             } else {
-                for(int k = query[i]; k < arr.length; k++) {
-                    list.add(arr[k]);
-                }
-                arr = list.stream().mapToInt(l -> l).toArray();
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
             }  
         } 
         
         int[] answer = arr;
 
         return answer;
+        
+//         1안
+//         for(int i = 0; i < query.length; i++) {
+//             List<Integer> list = new ArrayList<>();
+//             if(i % 2 == 0) {
+//                 for(int j = 0; j <= query[i]; j++) {
+//                     list.add(arr[j]);
+//                 }
+//                 arr = list.stream().mapToInt(l -> l).toArray();
+//             } else {
+//                 for(int k = query[i]; k < arr.length; k++) {
+//                     list.add(arr[k]);
+//                 }
+//                 arr = list.stream().mapToInt(l -> l).toArray();
+//             }  
+//         } 
+        
+//         int[] answer = arr;
+
+//         return answer;
     }
 }
