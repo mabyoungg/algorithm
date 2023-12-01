767560a3161001b2faa014a05f518de0e6c623f9
mabyoungg <mabyoungg@gmail.com>
2023-12-01 14:38:32 +0900

    [level unrated] Title: 2의 영역, Time: 2.49 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
+        int[] answer = {};
         List <Integer> list = new ArrayList<>();
-        List <Integer> list2 = new ArrayList<>();
         
         for (int i = 0; i < arr.length; i++){
             if (arr[i] == 2){
@@ -12,17 +12,39 @@ class Solution {
         }
         
         if (list.size() == 0){
-            list2.add(-1);
-        } else if (list.size() == 1){
-            list2.add(2);
-        } else {
-            for(int j = list.get(0); j <= list.get(list.size()-1); j++) {
-                list2.add(arr[j]);
-            }
+            return new int[]{-1};
         }
         
-        int[] answer = new int[list2.size()];
-        answer = list2.stream().mapToInt(k -> k).toArray();
+        int startIndex = list.get(0);
+        int endIndex = list.get(list.size()-1);
+        
+        answer = Arrays.copyOfRange(arr, startIndex, endIndex+1);
+        
         return answer;
+        
+        
+//         1안
+//         List <Integer> list = new ArrayList<>();
+//         List <Integer> list2 = new ArrayList<>();
+        
+//         for (int i = 0; i < arr.length; i++){
+//             if (arr[i] == 2){
+//                 list.add(i);
+//             }
+//         }
+        
+//         if (list.size() == 0){
+//             list2.add(-1);
+//         } else if (list.size() == 1){
+//             list2.add(2);
+//         } else {
+//             for(int j = list.get(0); j <= list.get(list.size()-1); j++) {
+//                 list2.add(arr[j]);
+//             }
+//         }
+        
+//         int[] answer = new int[list2.size()];
+//         answer = list2.stream().mapToInt(k -> k).toArray();
+//         return answer;
     }
 }
