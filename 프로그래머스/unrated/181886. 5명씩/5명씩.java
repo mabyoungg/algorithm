abc338fac3494c1be87757ef8f989d142fa09ed7
mabyoungg <mabyoungg@gmail.com>
2023-12-25 19:01:53 +0900

    [level unrated] Title: 5명씩, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,13 +1,27 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] names) {
-        String[] answer = names.length%5 == 0 ? new String[names.length/5] : new String[names.length/5 + 1];
-        int num = 0;
+        List<String> list = new ArrayList<>();
         
-        for (int i = 0; i < answer.length; i++){
-            answer[i] = names[num];
-            num+=5;
+        for (int i = 0; i < names.length; i+=5){
+            list.add(names[i]);
         }
-
+        
+        String[] answer = list.toArray(new String[list.size()]);
+        
         return answer;
+        
+//         1안
+//         String[] answer = names.length%5 == 0 ? new String[names.length/5] : new String[names.length/5 + 1];
+//         int num = 0;
+        
+//         for (int i = 0; i < answer.length; i++){
+//             answer[i] = names[num];
+//             num+=5;
+//         }
+
+//         return answer;
+        
     }
 }
