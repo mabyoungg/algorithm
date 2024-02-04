da3675a7e0d935bc2641260aa1c309e8ffe67d47
mabyoungg <mabyoungg@gmail.com>
2024-02-04 21:39:03 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.27 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,17 +2,22 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] myStringArray = my_string.split(" ");
-        List<String> myStringList = new ArrayList<>();
-        
-        for (int i = 0; i < myStringArray.length; i++) {
-            if (!myStringArray[i].equals("")) {
-                myStringList.add(myStringArray[i]);
-            }
-        }
-        
-        String[] answer = myStringList.toArray(new String[myStringList.size()]);
+        String[] answer = my_string.trim().split("\\s+");
         
         return answer;
+        
+//         1안
+//         String[] myStringArray = my_string.split(" ");
+//         List<String> myStringList = new ArrayList<>();
+        
+//         for (int i = 0; i < myStringArray.length; i++) {
+//             if (!myStringArray[i].equals("")) {
+//                 myStringList.add(myStringArray[i]);
+//             }
+//         }
+        
+//         String[] answer = myStringList.toArray(new String[myStringList.size()]);
+        
+//         return answer;
     }
 }
