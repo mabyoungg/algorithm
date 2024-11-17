993fab7af02b27e56daaca77c6de6ef8aed74c14
mabyoungg <mabyoungg@gmail.com>
2024-11-17 21:14:25 +0900

    [level 1] Title: 폰켓몬, Time: 6.21 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,13 +4,16 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set = Arrays.stream(nums)
-            .boxed()
-            .collect(Collectors.toCollection(HashSet::new));
+        return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length /2);
+            
+//         1안
+//         HashSet<Integer> set = Arrays.stream(nums)
+//             .boxed()
+//             .collect(Collectors.toCollection(HashSet::new));
         
-        int n = nums.length;
-        int k = n / 2;
+//         int n = nums.length;
+//         int k = n / 2;
         
-        return Math.min(k, set.size());
+//         return Math.min(k, set.size());
     }
 }
