218311bf87d5aaabc55b10bb5211eab0e66abbf7
mabyoungg <mabyoungg@gmail.com>
2024-12-25 19:25:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.12 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,20 +2,25 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        ArrayList<String> answerSort = new ArrayList<String>();
+        Arrays.sort(strings, (o1, o2) -> o1.charAt(n) == o2.charAt(n) ? o1.compareTo(o2) : Character.compare(o1.charAt(n), o2.charAt(n)));
         
-        for (int i = 0; i < strings.length; i++) {
-            answerSort.add(strings[i].charAt(n) + strings[i]);
-        }
+        return strings;
         
-        Collections.sort(answerSort);
+//         1안
+//         ArrayList<String> answerSort = new ArrayList<String>();
         
-        String[] answer = new String[answerSort.size()];
+//         for (int i = 0; i < strings.length; i++) {
+//             answerSort.add(strings[i].charAt(n) + strings[i]);
+//         }
         
-        for (int j = 0; j < answerSort.size(); j++) {
-           answer[j] = answerSort.get(j).substring(1);
-        }
+//         Collections.sort(answerSort);
+        
+//         String[] answer = new String[answerSort.size()];
+        
+//         for (int j = 0; j < answerSort.size(); j++) {
+//            answer[j] = answerSort.get(j).substring(1);
+//         }
 
-        return answer;
+//         return answer;
     }
 }
