a778efaf1b11f38f299318e798bdd13b6c61d133
mabyoungg <mabyoungg@gmail.com>
2025-01-10 23:04:59 +0900

    [level 2] Title: 전화번호 목록, Time: 266.44 ms, Memory: 244 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,29 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
+        
+        for (String s : phone_book) {
+            for (int i = 1; i < s.length(); i++) {
+                if (set.contains(s.substring(0, i))) {
+                    return false;
+                }
             }
         }
-
+        
         return true;
+        
+//         1안
+//         Arrays.sort(phone_book);
+
+//         for (int i = 0; i < phone_book.length - 1; i++) {
+//             if (phone_book[i + 1].startsWith(phone_book[i])) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
     }
 }
