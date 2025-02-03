d44367521a95c7e35827f7b1a1aef245c60b8855
mabyoungg <mabyoungg@gmail.com>
2025-02-04 07:42:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int num = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            num += numbers[i];
+        }
+        
+        answer = 45 - num;
+
+        return answer;
+    }
+}
